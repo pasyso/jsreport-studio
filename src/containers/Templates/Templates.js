79e3fc7a045c2e43d90b7bcf679e342581e311b1
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2016-03-30 15:48:12 +0200

    resizing on change

--- src/containers/Templates/Templates.js
+++ src/containers/Templates/Templates.js
@@ -26,8 +26,27 @@ export default class Templates extends Component {
     listAction: PropTypes.func.isRequired
   };
 
+  constructor() {
+    super()
+    this.handleClick = this.handleClick.bind(this)
+    this.handleContentChange = this.handleContentChange.bind(this)
+  }
+
   componentDidMount () {
     this.props.listAction()
+    var self = this;
+
+    this.refs.dockPane.dockManager.addLayoutListener({
+      onResumeLayout: function() {
+        self.refs.ace.editor.resize()
+      }
+    })
+
+    //setInterval(function() {
+    //  //self.forceUpdate()
+    //  self.refs.ace.editor.resize()
+    //  console.log(self.refs.ace)
+    //}, 5000)
   }
 
   componentDidUpdate () {
@@ -38,32 +57,50 @@ export default class Templates extends Component {
     preview(this.props.list[ 0 ], 'previewFrame')
   }
 
+  handleContentChange (val) {
+    this.props.list[0].content = val
+  }
+
   render () {
     const { list, loaded, error} = this.props
     const template = list.length ? list[ 0 ] : { content: 'loading' }
     return (
       <div>
         <div>
-          <Button bsStyle='success' onClick={() => this.handleClick()}>Run-zz {loaded}</Button>
+          <Button bsStyle='success' onClick={this.handleClick}>Run-zz {loaded}</Button>
         </div>
-        <DockPane>
-          <DockPanel position='fill'>
-            <AceEditor
+        <DockPane ref='dockPane'>
+          <DockPanel position='fill' title='template content'>
+            <AceEditor ref='ace'
               mode='handlebars'
+              onChange={this.handleContentChange}
               theme='chrome'
               width='100%'
               height='100%'
-              name='UNIQUE_ID_OF_DIV'
+              name='UNIQUE_ID_2'
               value={template.content}
-              $blockScrolling='Infinity'
+              editorProps={{$blockScrolling: true}}
               />
+
+            <DockPanel position='right' title='preview' ratio={0.5}>
+              <div className={style.previewPaneWrap}>
+                <iframe name='previewFrame' className={style.previewPane} allowTransparency='true' frameBorder='0' allowFullScreen='true'></iframe>
+              </div>
+            </DockPanel>
+
+            <DockPanel position='down' title='template helpers' ratio={0.5}>
+              <AceEditor
+                mode='javascript'
+                theme='chrome'
+                width='100%'
+                height='100%'
+                name='UNIQUE_ID_1'
+                value={template.helpers}
+                editorProps={{$blockScrolling: true}}
+                />
+            </DockPanel>
           </DockPanel>
-          <DockPanel position='right'>
-            <div className={style.previewPaneWrap}>
-              <iframe id="iframe" name='previewFrame' className={style.previewPane} allowTransparency='true'
-                      frameBorder='0' allowFullScreen='true'></iframe>
-            </div>
-          </DockPanel>
+
         </DockPane>
       </div>
     )
