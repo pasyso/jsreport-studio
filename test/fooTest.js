ba2da3b0f6ddd228239eb16c9da6b9e51e04ba47
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2016-04-06 17:47:25 +0200

    try first test

---
+++ test/fooTest.js
@@ -0,0 +1,8 @@
+import 'should'
+import * as actions from '../src/redux/modules/editor.js'
+
+describe('actions', () => {
+  it('should create an action to add a todo', () => {
+    actions.should.be.ok
+  })
+})
