a8938d912d9645ae85d38c7fa06567d3695f9fa7
honza.pofider@seznam.cz <honza.pofider@seznam.cz>
2016-04-11 09:56:26 +0200

    avoid webstorm complains

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,5 +1,10 @@
-export App from './App/App'
-export Home from './Home/Home'
-export Studio from './Studio/Studio'
-export NotFound from './NotFound/NotFound'
+import _App from './App/App'
+import _Studio from './Studio/Studio'
+import _Home from './Home/Home'
+import _NotFound from './NotFound/NotFound'
+
+export const App = _App
+export const Studio = _Studio
+export const Home = _Home
+export const NotFound = _NotFound
 
