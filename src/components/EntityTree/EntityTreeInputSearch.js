dd077928b4d1991bbd38086de55119ddf09cf18f
BJR Matos <bjrmatos@gmail.com>
2017-01-24 13:28:34 -0500

    encapsulate input search in Entity Tree into a button

--- src/components/EntityTree/EntityTreeInputSearch.js
+++ src/components/EntityTree/EntityTreeInputSearch.js
@@ -1,9 +1,55 @@
+import React, { Component } from 'react'
+import Popover from '../../components/common/Popover'
+import EntityTreeButton from './EntityTreeButton'
 import style from './EntityTreeInputSeach.scss'
 
-export default ({ setFilter }) => {
-  return (
-    <div className={style.search}>
-      <input type='text' onChange={(ev) => setFilter({ name: ev.target.value })} />
-    </div>
-  )
+class EntityTreeInputSeach extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      displayInput: false,
+      filterActive: false,
+      filterByName: ''
+    }
+
+    this.onNameFilterChange = this.onNameFilterChange.bind(this)
+  }
+
+  onNameFilterChange (ev) {
+    const name = ev.target.value
+
+    this.setState({
+      filterActive: Boolean(name),
+      filterByName: name
+    })
+
+    this.props.setFilter({ name })
+  }
+
+  render () {
+    const { displayInput, filterActive, filterByName } = this.state
+
+    return (
+      <div title='Filter entities tree by name' className={style.container}>
+        <EntityTreeButton active={filterActive} onClick={() => this.setState({ displayInput: true })}>
+          <span style={{ display: 'inline-block' }}>
+            <i className='fa fa-filter' />
+            &nbsp;
+            <i className='fa fa-font' />
+          </span>
+        </EntityTreeButton>
+        <Popover
+          open={displayInput}
+          onClose={() => this.setState({ displayInput: false })}
+        >
+          <div className={style.search}>
+            <input type='text' value={filterByName} onChange={this.onNameFilterChange} />
+          </div>
+        </Popover>
+      </div>
+    )
+  }
 }
+
+export default EntityTreeInputSeach
