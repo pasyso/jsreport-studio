b048ae93f0e862e323f307e382a2061675db6403
BJR Matos <bjrmatos@gmail.com>
2018-04-20 14:22:56 -0500

    add optionsSchema

--- jsreport.config.js
+++ jsreport.config.js
@@ -1,5 +1,19 @@
+
 module.exports = {
   'name': 'studio',
   'main': 'lib/studio.js',
+  'optionsSchema': {
+    extensions: {
+      studio: {
+        type: 'object',
+        properties: {
+          entityTreeOrder: {
+            type: 'array',
+            items: { type: 'string' }
+          }
+        }
+      }
+    }
+  },
   'dependencies': ['express']
 }
